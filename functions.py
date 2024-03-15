79e1770550e918fd1d9fc61d8262c6e0511c7c03
rturquier <remi.turquier@ensae.fr>
2024-03-15 16:36:44 +0100

    🛠️  Refactor: put Drupp functions in other file

---
+++ functions.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Functions used for "Ethically robust discounting"
+"""
+
+
+import numpy as np
+import pandas as pd
+import altair as alt
+
+
+def ramsey_beta_prime_gamma(tau, g, beta_prime, alpha, beta,
+                            alpha_prime = 1, c_0=1,):
+    """Return the social discount rate using equation (x).
+
+    Arguments
+    ---------
+    tau         -- time period
+    g           -- consumption growth rate
+    alpha_prime -- first shape parameter of the beta prime distribution
+                   only alpha_prime = 1 is supported
+    beta_prime  -- second shape parameter of the beta prime distribution
+    alpha       --  shape parameter of the gamma distribution
+    beta        -- rate parameter of the gamma distribution
+    c_0         -- consumption at tau = 0 (default 1)
+    """
+    discount_rate = (
+        (1 / tau) * np.log(1 + tau / beta_prime)
+        + (alpha / tau)
+        * np.log(1 + tau * np.log(1 + g) / (beta + np.log(c_0)))
+    )
+    return discount_rate
+
+
+def ramsey(delta, eta, g):
+    return delta + eta * g
+
+
+def exact_sdr_beta_prime_gamma(tau, g, beta_prime, alpha, beta,
+                               alpha_prime = 1, c_0=1):
+    """Return the exact social discount rate using equation (x).
+
+    Arguments
+    ---------
+    tau         -- time period
+    g           -- consumption growth rate
+    alpha_prime -- first shape parameter of the beta prime distribution
+                   only alpha_prime = 1 is supported
+    beta_prime  -- second shape parameter of the beta prime distribution
+    alpha       --  shape parameter of the gamma distribution
+    beta        -- rate parameter of the gamma distribution
+    c_0         -- consumption at tau = 0 (default 1)
+    """
+    discount_rate = (
+        (1 + tau / beta_prime)**(1 / tau)
+        * (
+            1 + (tau * np.log(1 + g) / (beta + np.log(c_0)))
+        )**(alpha / tau)
+        - 1
+    )
+    return discount_rate
+
+
+def sdr_to_sdf(sdr, tau):
+    discount_factor = (1 / (1 + sdr))**tau
+    return discount_factor
+
+
+def exact_sdf_beta_prime_gamma(tau, g, beta_prime, alpha, beta,
+                               alpha_prime = 1, c_0=1):
+    """Return the exact social discount factor using equation (x).
+
+    Arguments
+    ---------
+    tau         -- time period
+    g           -- consumption growth rate
+    alpha_prime -- first shape parameter of the beta prime distribution
+                   only alpha_prime = 1 is supported
+    beta_prime  -- second shape parameter of the beta prime distribution
+    alpha       --  shape parameter of the gamma distribution
+    beta        -- rate parameter of the gamma distribution
+    c_0         -- consumption at tau = 0 (default 1)
+    """
+    sdr = exact_sdr_beta_prime_gamma(tau, g, beta_prime, alpha, beta,
+                                     alpha_prime, c_0)
+    discount_factor = sdr_to_sdf(sdr, tau)
+    return discount_factor
+
+
+def x_axis_from_series(series, n_steps_min=200):
+    series = series.dropna()
+    max_value = series.max()
+    axis = np.linspace(0, max_value, n_steps_min)
+    axis = sorted(set(axis) | set(series))
+
+    assert(set(series).issubset(axis))
+
+    return axis
+
+
+def merge_value_frequency(df_x, col_x, df_y, col_y):
+    """
+    Add frequencies of ``df_x`` values observed in ``df_y``.
+
+    Parameters
+    ----------
+    df_x : DataFrame
+        DataFrame containing a list of unique values.
+    col_x : str
+        Name of the column of ``df_x`` containing a list of unique values.
+    df_y : DataFrame
+        DataFrame containing a list of observations
+    col_y : str
+        Name of the column of ``df_y`` containing a list of observations.
+
+
+    Returns
+    -------
+    DataFrame
+        ``df_x`` with an additional column called "freq_" + ``col_y``,
+        containing the frequency of each unique value of ``col_x`` observed in
+        ``col_y``.
+    """
+    df = (
+        df_y
+        .loc[:, col_y]
+        .value_counts(normalize=True)
+        .pipe(pd.DataFrame)
+        .rename(columns={col_y: "freq_" + col_y})
+        .merge(df_x, left_index=True, right_on=col_x, how="right")
+        .fillna({"freq_" + col_y: 0})
+        .reindex(columns=[col_x, "freq_" + col_y])
+    )
+
+    return df
+
+
+def density_chart(df, x, freq, pdf, bin_step=1, x_format="~f"):
+    """Plot a histogram with a fitted probability density function."""
+
+    bar_color  = alt.value("#8FBC8F")
+    line_color = alt.value("#008B8B")
+
+    bar_x     = alt.X(x, bin=alt.Bin(step=bin_step), axis=alt.Axis(title=""))
+    bar_scale = 'datum.' + freq + ' / ' + str(bin_step)
+    bar_y     = alt.Y('sum(freq_scaled):Q', axis=alt.Axis(title=""))
+
+    line_x = alt.X(x,   axis=alt.Axis(title="value", format=x_format))
+    line_y = alt.Y(pdf, axis=alt.Axis(title="density"), type="quantitative")
+
+
+    base = alt.Chart(df)
+
+    bar = (
+        base.mark_bar()
+        .transform_calculate(freq_scaled=bar_scale)
+        .encode(x=bar_x, y=bar_y, color=bar_color)
+    )
+
+    line = (
+        base.mark_line()
+        .encode(x=line_x, y=line_y, color=line_color)
+    )
+
+    return bar + line
+
+
+def dict_to_labelExpr(legend_dict):
+    """Convert a dictonary to a labelExpr vega expression."""
+
+    labelExpr = ""
+
+    for index, variable_name in enumerate(legend_dict):
+        labelExpr += f"datum.label == '{variable_name}' "
+        labelExpr += f"? '{legend_dict[variable_name]}' : "
+
+        if index + 1 == len(legend_dict):
+            labelExpr += f"'unspecified label'"
+
+    return labelExpr
+
+
+def line_chart(df, x, y, x_title=False, y_title=False, x_format="~f",
+               y_format="~f", y_scale=alt.Scale(), title="", subtitle="",
+               color="#1f77b4", strokeDash=[1, 0], multi=False,
+               legend=alt.Legend(), legend_order=None):
+    """Plot a simple line chart."""
+
+    if x_title == False:
+        x_title = x
+    if y_title == False:
+        y_title = y
+
+    chart = (
+        alt.Chart(df)
+        .mark_line()
+        .encode(
+            x=alt.X(x, axis=alt.Axis(title=x_title, format=x_format)),
+            y=alt.Y(y, axis=alt.Axis(title=y_title, format=y_format),
+                    scale=y_scale)
+        )
+        .properties(title={"text": title, "subtitle": subtitle})
+    )
+
+    if multi:
+        chart = chart.encode(
+            color=alt.Color(color, legend=legend, sort=legend_order),
+            strokeDash=alt.StrokeDash(strokeDash, legend=legend,
+                                      sort=legend_order)
+        )
+    else:
+        chart = chart.encode(
+            color=alt.value(color),
+            strokeDash=alt.value(strokeDash)
+        )
+
+    return chart
+
--- numerical_application.py
+++ numerical_application.py
@@ -2,237 +2,31 @@
 # -*- coding: utf-8 -*-
 
 """
-Code used for the internship report
+Analyse survey data from Drupp et al. (2018a)
 
-Required files: Dataset_Stata11compatible.dta (Drupp et al. 2018)
+Required files: Dataset_Stata11compatible.dta (Drupp et al. 2018b)
 
 
 -- References --
-Drupp, Moritz A., Freeman, Mark C., Groom, Ben, and Nesje, Frikk. Replication
-data for: Discounting Disentangled. Nashville, TN: American Economic
-Association [publisher], 2018. Ann Arbor, MI: Inter-university Consortium for
-Political and Social Research [distributor], 2019-10-13.
+Drupp, Moritz A., Mark C. Freeman, Ben Groom, and Frikk Nesje. 2018a.
+'Discounting Disentangled'. American Economic Journal: Economic Policy.
+https://doi.org/10.1257/pol.20160240.
+
+Drupp, Moritz A., Freeman, Mark C., Groom, Ben, and Nesje, Frikk. 2018b.
+Replication data for: Discounting Disentangled.
 https://doi.org/10.3886/E114692V1
 """
 
-# %% =======================   Setup   =========================
+# %% Imports
 import numpy as np
 import pandas as pd
 import scipy.stats as stats
 import altair as alt
 
-
-# %% ======================= Functions =========================
-def ramsey_beta_prime_gamma(tau, g, beta_prime, alpha, beta,
-                            alpha_prime = 1, c_0=1,):
-    """Return the social discount rate using equation (x).
-
-    Arguments
-    ---------
-    tau         -- time period
-    g           -- consumption growth rate
-    alpha_prime -- first shape parameter of the beta prime distribution
-                   only alpha_prime = 1 is supported
-    beta_prime  -- second shape parameter of the beta prime distribution
-    alpha       --  shape parameter of the gamma distribution
-    beta        -- rate parameter of the gamma distribution
-    c_0         -- consumption at tau = 0 (default 1)
-    """
-    discount_rate = (
-        (1 / tau) * np.log(1 + tau / beta_prime)
-        + (alpha / tau)
-        * np.log(1 + tau * np.log(1 + g) / (beta + np.log(c_0)))
-    )
-    return discount_rate
+import functions as f
 
 
-def ramsey(delta, eta, g):
-    return delta + eta * g
-
-
-def exact_sdr_beta_prime_gamma(tau, g, beta_prime, alpha, beta,
-                               alpha_prime = 1, c_0=1):
-    """Return the exact social discount rate using equation (x).
-
-    Arguments
-    ---------
-    tau         -- time period
-    g           -- consumption growth rate
-    alpha_prime -- first shape parameter of the beta prime distribution
-                   only alpha_prime = 1 is supported
-    beta_prime  -- second shape parameter of the beta prime distribution
-    alpha       --  shape parameter of the gamma distribution
-    beta        -- rate parameter of the gamma distribution
-    c_0         -- consumption at tau = 0 (default 1)
-    """
-    discount_rate = (
-        (1 + tau / beta_prime)**(1 / tau)
-        * (
-            1 + (tau * np.log(1 + g) / (beta + np.log(c_0)))
-        )**(alpha / tau)
-        - 1
-    )
-    return discount_rate
-
-
-def sdr_to_sdf(sdr, tau):
-    discount_factor = (1 / (1 + sdr))**tau
-    return discount_factor
-
-
-def exact_sdf_beta_prime_gamma(tau, g, beta_prime, alpha, beta,
-                               alpha_prime = 1, c_0=1):
-    """Return the exact social discount factor using equation (x).
-
-    Arguments
-    ---------
-    tau         -- time period
-    g           -- consumption growth rate
-    alpha_prime -- first shape parameter of the beta prime distribution
-                   only alpha_prime = 1 is supported
-    beta_prime  -- second shape parameter of the beta prime distribution
-    alpha       --  shape parameter of the gamma distribution
-    beta        -- rate parameter of the gamma distribution
-    c_0         -- consumption at tau = 0 (default 1)
-    """
-    sdr = exact_sdr_beta_prime_gamma(tau, g, beta_prime, alpha, beta,
-                                     alpha_prime, c_0)
-    discount_factor = sdr_to_sdf(sdr, tau)
-    return discount_factor
-
-
-def x_axis_from_series(series, n_steps_min=200):
-    series = series.dropna()
-    max_value = series.max()
-    axis = np.linspace(0, max_value, n_steps_min)
-    axis = sorted(set(axis) | set(series))
-
-    assert(set(series).issubset(axis))
-
-    return axis
-
-
-def merge_value_frequency(df_x, col_x, df_y, col_y):
-    """
-    Add frequencies of ``df_x`` values observed in ``df_y``.
-
-    Parameters
-    ----------
-    df_x : DataFrame
-        DataFrame containing a list of unique values.
-    col_x : str
-        Name of the column of ``df_x`` containing a list of unique values.
-    df_y : DataFrame
-        DataFrame containing a list of observations
-    col_y : str
-        Name of the column of ``df_y`` containing a list of observations.
-
-
-    Returns
-    -------
-    DataFrame
-        ``df_x`` with an additional column called "freq_" + ``col_y``,
-        containing the frequency of each unique value of ``col_x`` observed in
-        ``col_y``.
-    """
-    df = (
-        df_y
-        .loc[:, col_y]
-        .value_counts(normalize=True)
-        .pipe(pd.DataFrame)
-        .rename(columns={col_y: "freq_" + col_y})
-        .merge(df_x, left_index=True, right_on=col_x, how="right")
-        .fillna({"freq_" + col_y: 0})
-        .reindex(columns=[col_x, "freq_" + col_y])
-    )
-
-    return df
-
-
-def density_chart(df, x, freq, pdf, bin_step=1, x_format="~f"):
-    """Plot a histogram with a fitted probability density function."""
-
-    bar_color  = alt.value("#8FBC8F")
-    line_color = alt.value("#008B8B")
-
-    bar_x     = alt.X(x, bin=alt.Bin(step=bin_step), axis=alt.Axis(title=""))
-    bar_scale = 'datum.' + freq + ' / ' + str(bin_step)
-    bar_y     = alt.Y('sum(freq_scaled):Q', axis=alt.Axis(title=""))
-
-    line_x = alt.X(x,   axis=alt.Axis(title="value", format=x_format))
-    line_y = alt.Y(pdf, axis=alt.Axis(title="density"), type="quantitative")
-
-
-    base = alt.Chart(df)
-
-    bar = (
-        base.mark_bar()
-        .transform_calculate(freq_scaled=bar_scale)
-        .encode(x=bar_x, y=bar_y, color=bar_color)
-    )
-
-    line = (
-        base.mark_line()
-        .encode(x=line_x, y=line_y, color=line_color)
-    )
-
-    return bar + line
-
-
-def dict_to_labelExpr(legend_dict):
-    """Convert a dictonary to a labelExpr vega expression."""
-
-    labelExpr = ""
-
-    for index, variable_name in enumerate(legend_dict):
-        labelExpr += f"datum.label == '{variable_name}' "
-        labelExpr += f"? '{legend_dict[variable_name]}' : "
-
-        if index + 1 == len(legend_dict):
-            labelExpr += f"'unspecified label'"
-
-    return labelExpr
-
-
-def line_chart(df, x, y, x_title=False, y_title=False, x_format="~f",
-               y_format="~f", y_scale=alt.Scale(), title="", subtitle="",
-               color="#1f77b4", strokeDash=[1, 0], multi=False,
-               legend=alt.Legend(), legend_order=None):
-    """Plot a simple line chart."""
-
-    if x_title == False:
-        x_title = x
-    if y_title == False:
-        y_title = y
-
-    chart = (
-        alt.Chart(df)
-        .mark_line()
-        .encode(
-            x=alt.X(x, axis=alt.Axis(title=x_title, format=x_format)),
-            y=alt.Y(y, axis=alt.Axis(title=y_title, format=y_format),
-                    scale=y_scale)
-        )
-        .properties(title={"text": title, "subtitle": subtitle})
-    )
-
-    if multi:
-        chart = chart.encode(
-            color=alt.Color(color, legend=legend, sort=legend_order),
-            strokeDash=alt.StrokeDash(strokeDash, legend=legend,
-                                      sort=legend_order)
-        )
-    else:
-        chart = chart.encode(
-            color=alt.value(color),
-            strokeDash=alt.value(strokeDash)
-        )
-
-    return chart
-
-
-# %% ======================= Main code =========================== #
-# %% Import data from Drupp et al. (2018)
+# %% Read data from Drupp et al. (2018b)
 drupp_2018_data_path = "114692-V1/data/Dataset_Stata11compatible.dta"
 drupp_2018_df = pd.read_stata(drupp_2018_data_path)
 
@@ -246,11 +40,11 @@ drupp_2018_df = (drupp_2018_df
 drupp_2018_df.head()
 
 # %% Prepare dataframes for the charts
-df_delta = pd.DataFrame({'x_delta': x_axis_from_series(drupp_2018_df.delta)})
-df_eta   = pd.DataFrame({'x_eta':   x_axis_from_series(drupp_2018_df.eta  )})
+df_delta = pd.DataFrame({'x_delta': f.x_axis_from_series(drupp_2018_df.delta)})
+df_eta   = pd.DataFrame({'x_eta':   f.x_axis_from_series(drupp_2018_df.eta  )})
 
-df_delta = merge_value_frequency(df_delta, "x_delta", drupp_2018_df, "delta")
-df_eta   = merge_value_frequency(df_eta, "x_eta", drupp_2018_df, "eta")
+df_delta = f.merge_value_frequency(df_delta, "x_delta", drupp_2018_df, "delta")
+df_eta   = f.merge_value_frequency(df_eta, "x_eta", drupp_2018_df, "eta")
 
 df_delta.head()
 
@@ -307,7 +101,7 @@ df_eta = df_eta.assign(
 
 # %% Density charts - delta
 (
-    density_chart(df_delta, x="x_delta", freq="freq_delta",
+    f.density_chart(df_delta, x="x_delta", freq="freq_delta",
                   pdf="pdf_delta_MLE", bin_step=0.004, x_format="%"
                  )
     .properties(title={"text": "Distribution of beliefs over \u03b4",
@@ -317,7 +111,7 @@ df_eta = df_eta.assign(
 
 
 (
-    density_chart(df_delta, x="x_delta", freq="freq_delta",
+    f.density_chart(df_delta, x="x_delta", freq="freq_delta",
                   pdf="pdf_delta_MM", bin_step=0.004, x_format="%"
                  )
     .properties(title={"text": "Distribution of beliefs over \u03b4",
@@ -328,7 +122,7 @@ df_eta = df_eta.assign(
 
 # %% Density charts - eta
 (
-    density_chart(df_eta, x="x_eta", freq="freq_eta", pdf="pdf_eta_MLE",
+    f.density_chart(df_eta, x="x_eta", freq="freq_eta", pdf="pdf_eta_MLE",
                   bin_step=0.4)
     .properties(title={"text": "Distribution of beliefs over \u03b7",
                        "subtitle": "Fit with maximum likelihood estimation"})
@@ -337,7 +131,7 @@ df_eta = df_eta.assign(
 
 
 (
-    density_chart(df_eta, x="x_eta", freq="freq_eta", pdf="pdf_eta_MM",
+    f.density_chart(df_eta, x="x_eta", freq="freq_eta", pdf="pdf_eta_MM",
                   bin_step=0.4)
     .properties(title={"text": "Distribution of beliefs over \u03b7",
                        "subtitle": "Fit with method of moments"})
@@ -353,35 +147,35 @@ df_sdr = (
     pd.DataFrame()
     .assign(
         year=np.linspace(1, 500, 300),
-        sdr_uncertain_approx=lambda r: ramsey_beta_prime_gamma(
+        sdr_uncertain_approx=lambda r: f.ramsey_beta_prime_gamma(
             tau=r.year, g=0.02, beta_prime=beta_prime_MM,
             alpha=gamma_MM_shape, beta=1 / gamma_MM_scale
         ),
-        sdr_uncertain_exact=lambda r: exact_sdr_beta_prime_gamma(
+        sdr_uncertain_exact=lambda r: f.exact_sdr_beta_prime_gamma(
             tau=r.year, g=0.02, beta_prime=beta_prime_MM,
             alpha=gamma_MM_shape, beta=1 / gamma_MM_scale
         ),
         sdr_uncertain_error=lambda r:
             (r.sdr_uncertain_exact - r.sdr_uncertain_approx)
             / r.sdr_uncertain_approx,
-        sdr_certain_ramsey=ramsey(delta=m, eta=mu, g=0.02),
-        sdr_favorite_theory=ramsey(
+        sdr_certain_ramsey=f.ramsey(delta=m, eta=mu, g=0.02),
+        sdr_favorite_theory=f.ramsey(
             delta=favorite_delta, eta=favorite_eta, g=0.02
         ),
         sdf_uncertain=lambda r:
-            sdr_to_sdf(r.sdr_uncertain_exact, r.year),
+            f.sdr_to_sdf(r.sdr_uncertain_exact, r.year),
         sdf_certain=lambda r:
-            sdr_to_sdf(r.sdr_certain_ramsey, r.year),
+            f.sdr_to_sdf(r.sdr_certain_ramsey, r.year),
         sdf_ratio=lambda r: r.sdf_uncertain / r.sdf_certain,
         sdf_favorite_theory=lambda r:
-            sdr_to_sdf(r.sdr_favorite_theory, r.year),
+            f.sdr_to_sdf(r.sdr_favorite_theory, r.year),
     )
 )
 
 # %% Plot relative approximation error
 (
     df_sdr
-    .pipe(line_chart, x="year", y="sdr_uncertain_error",
+    .pipe(f.line_chart, x="year", y="sdr_uncertain_error",
           x_title="Year",
           y_title="Relative approximation error", y_format="%")
     .properties(width=600, height=300)
@@ -405,14 +199,14 @@ legend_dict = {
     ]]
     .melt("year")
     .pipe(
-        line_chart, x="year", y="value",
+        f.line_chart, x="year", y="value",
         color="variable",
         strokeDash="variable",
         multi=True,
         # title = "Long-run social discount rate",
         legend=alt.Legend(
             title=None,
-            labelExpr=dict_to_labelExpr(legend_dict),
+            labelExpr=f.dict_to_labelExpr(legend_dict),
             orient="bottom"
         ),
         x_title="Year",
@@ -435,14 +229,14 @@ legend_dict = {
     .loc[:, ["year", "sdf_uncertain", "sdf_certain", "sdf_favorite_theory"]]
     .melt("year")
     .pipe(
-        line_chart, x="year", y="value",
+        f.line_chart, x="year", y="value",
         multi=True,
         color="variable",
         strokeDash="variable",
         # title = "Long-run social discount factor",
         legend=alt.Legend(
             title=None,
-            labelExpr=dict_to_labelExpr(legend_dict),
+            labelExpr=f.dict_to_labelExpr(legend_dict),
             orient="bottom"
         ),
         x_title="Year",
@@ -458,7 +252,7 @@ legend_dict = {
 (
     df_sdr
     .pipe(
-        line_chart, x="year", y="sdf_ratio",
+        f.line_chart, x="year", y="sdf_ratio",
         y_scale=alt.Scale(type="log"),
         x_title="Year",
         y_title="Morally uncertain discount factor over standard factor",
