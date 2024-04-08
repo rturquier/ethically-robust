464fbfe14394272e6653f474430fb987405b05ca
rturquier <remi.turquier@ensae.fr>
2024-04-08 21:10:23 +0200

    🛝  Add sliders

--- survey/survey.js
+++ survey/survey.js
@@ -1,3 +1,10 @@
 import beta from 'https://cdn.jsdelivr.net/gh/stdlib-js/stats-base-dists-beta@esm/index.mjs';
 
-console.log(beta);
+let vizBeliefs = document.getElementById("viz-beliefs");
+
+// vizBeliefs.innerHTML = beta.pdf(0.5, 5, 5);
+sliderBeliefsAlpha = document.querySelector("#viz-beliefs ~ input.alpha")
+sliderBeliefsBeta = document.querySelector("#viz-beliefs ~ input.beta")
+
+
+
