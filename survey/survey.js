fe3082a310041cd267e56e578cabe3f4435784a2
rturquier <remi.turquier@ensae.fr>
2024-04-23 12:05:38 +0200

    🍠  Plot basic beta distribution

--- survey/survey.js
+++ survey/survey.js
@@ -1,18 +1,21 @@
-import beta from 'https://cdn.jsdelivr.net/gh/stdlib-js/stats-base-dists-beta@esm/index.mjs';
+import { pdf as betaPdf } from 'https://cdn.jsdelivr.net/gh/stdlib-js/stats-base-dists-beta@esm/index.mjs';
+
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 
-function plotBetaPdf(plotSelector, alpha, beta){
-    let viz = document.querySelector(plotSelector);
-    let sliderAlpha = document.querySelector(plotSelector + "~ input.alpha");
-    let sliderBeta = document.querySelector(plotSelector + "~ input.beta");
 
-    const width = 640;
-    const height = 300;
+function plotBetaPdf(plotSelector){
+    const viz = document.querySelector(plotSelector);
+    const sliderAlpha = document.querySelector(plotSelector + "~ input.alpha");
+    const sliderBeta = document.querySelector(plotSelector + "~ input.beta");
+
+    const width = viz.offsetWidth;
+    const height = width / 1.8;
     const marginTop = 20;
     const marginRight = 20;
     const marginBottom = 30;
     const marginLeft = 40;
+    const xPrecision = 0.01;
 
     let svg = d3.create("svg")
                 .attr("width", width)
@@ -34,6 +37,28 @@ function plotBetaPdf(plotSelector, alpha, beta){
         .attr("transform", `translate(${marginLeft},0)`)
         .call(d3.axisLeft(yScale));
 
+    let alpha = sliderAlpha.valueAsNumber;
+    let beta = sliderBeta.valueAsNumber;
+    const points = [];
+    for (let x = 0; x <= 1; x += xPrecision){
+        let y = betaPdf(x, alpha, beta);
+        points.push([x, y]);
+    }
+    
+    console.log(points);
+
+    let line = d3.line()
+                .x(d => xScale(d[0]))
+                .y(d => yScale(d[1]));
+    
+    svg.append("path")
+        .datum(points)
+        .attr("clip-path", "url(" + plotSelector + ")")
+        .attr("fill", "none")
+        .attr("stroke", "teal")
+        .attr("stroke-width", 2)
+        .attr("d", line);
+                            
     viz.append(svg.node());
 }
 
