7191e198279e9e6b23a7002d6df933f33d01c9c6
rturquier <remi.turquier@ensae.fr>
2024-07-17 12:34:35 +0200

    ✋  Fetch missing abstracts manually from website

--- survey_recipients.py
+++ survey_recipients.py
@@ -5,8 +5,8 @@
 Get list of potential experts for population ethics survey
 
 Todo:
-- exclude papers that only match the query because of punctuation like
-  "...population, ethics..."
+- exclude "repugnant conclusions" (with an s), which are sometimes not
+  about population ethics
 """
 
 # %% Imports
@@ -20,7 +20,7 @@ params = {
              '"total utilitarianism"|"repugnant conclusion"',
     'limit': "5",
     'fields': "authors,year,title,abstract,paperId,s2FieldsOfStudy," +
-              "externalIds,venue,publicationVenue",
+              "externalIds,venue,publicationVenue,url",
     'year': "2000-",
     'fieldsOfStudy': "Economics,Philosophy,Sociology,Political Science"
 }
@@ -29,7 +29,8 @@ response_df = pd.json_normalize(response.json()['data'])
 
 # %% Tidy data
 relevant_columns = ['name', 'year', 'title', 'venue', 'publicationVenue',
-                    'abstract', 's2FieldsOfStudy', 'authorId', 'paperId']
+                    'abstract', 's2FieldsOfStudy', "url", 'authorId',
+                    'paperId']
 
 long_df = response_df.explode('authors', ignore_index=True)
 authors_name_and_id = long_df.authors.pipe(pd.json_normalize)
@@ -58,14 +59,49 @@ query_condition = (
     + "or abstract.str.contains(@keyword_pattern, case=False)"
 )
 
-selected_authors_df = authors_df.query(query_condition)
-
 all_authors = authors_df.name.drop_duplicates()
-selected_authors = selected_authors_df.name.drop_duplicates()
-excluded_authors = (
-  authors_df[~authors_df.name.isin(selected_authors)]
-  .name
-  .drop_duplicates()
+first_author_selection = (
+    authors_df
+    .query(query_condition)
+    .name
+    .drop_duplicates()
 )
 
-assert len(all_authors) == len(selected_authors) + len(excluded_authors)
+# %% Refine selection using missing abstracts
+# For some papers, "due to legal reasons", the SemanticScholar API doesn't
+# return the abstract, even though they are online. For some of these papers,
+# we need to retrieve the abstract manually in order to check if it contains
+# the precise search terms of the query (eg. "population ethics" and not
+# "population. Ethics").
+
+abstract_is_empty = "~ abstract.str.contains('\w').astype('bool')"
+empty_abstracts_df = (
+    authors_df
+    .query(abstract_is_empty)
+    # no need to fetch abstracts for authors we already selected
+    .query("~ name.isin(@first_author_selection)") 
+    .drop_duplicates("paperId")
+)
+
+# Export table and manually copy abstracts from the SemanticScholar website,
+# following the links in the "url" column. Takes about 15 minutes.
+# empty_abstracts_df.to_csv("data/empty_abstracts_df.csv")
+
+manually_fetched_abstracts_df = (
+    pd.read_csv(
+        "data/manually_fetched_abstracts_df.csv", sep=";", index_col=0
+    )
+    [['abstract', 'paperId']]
+)
+
+all_abstracts_df = (
+    pd.merge(
+        authors_df,
+        manually_fetched_abstracts_df,
+        on="paperId",
+        how="left"
+    )
+    .assign(abstract = lambda df: df.abstract_y.combine_first(df.abstract_x))
+)    
+
+selected_authors_df = all_abstracts_df.query(query_condition)
